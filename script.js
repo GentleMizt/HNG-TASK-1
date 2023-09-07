af9d6669c81821e9de968509244a2680a8ac95af
Mapelujo <olamideiyanda18@gmail.com>
2023-09-07 15:17:28 +0100

    progress

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello world');
+const currentDay = document.getElementById('day');
+const timeInMilli = document.getElementById('time');
+
+// Function to display current Day of the week
+const displayCurrentDayOfWeek = () =>{
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    const currentDate = new Date();
+    const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+    currentDay.textContent = `Current Day of the Week: ${dayOfWeek}`;
+}
+
+// Function to display Current time in Milliseconds
+
+displayCurrentDayOfWeek();
